7d1073bfa0cfb5659d82043fd7cd3c7339feca53
Ricky Duong <huuphuoc29791@gmail.com>
2026-08-26 20:48:56 +0700

    Node buổi 02 (26/08) - Xử lý form, render giao diện

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const app = express();
-const { getAllProducts } = require('./products');
+const products = require('./data/products');
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	res.render('home', { name: 'Ricky', age: 30, hour: 10 });
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/products', (req, res) => {
-	res.render('products', { products: getAllProducts() });
+	res.render('products/productList', {
+		products
+	});
 });
 
-app.get('/abc', (req, res) => {
-	res.redirect('http://www.google.com');
+app.get('/products/create', (req, res) => {
+	res.render('products/productCreate');
+});
+
+app.post('/products/create', (req, res) => {
+	const { id, name, price, description } = req.body;
+	products.push({
+		id: Number(id),
+		name,
+		price: Number(price),
+		description
+	});
+	res.redirect('/products');
 });
 
 app.listen(8000, () => {
